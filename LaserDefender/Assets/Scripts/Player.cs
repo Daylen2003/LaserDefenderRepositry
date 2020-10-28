b83423805a072b3caee52b47c89d26dba1ed3955
Daylen2003 <73096438+Daylen2003@users.noreply.github.com>
2020-10-28 16:46:44 +0100

    Updated Game
    Movement of player

--- LaserDefender/Assets/Scripts/Player.cs
+++ LaserDefender/Assets/Scripts/Player.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Player : MonoBehaviour
 {
@@ -13,6 +15,20 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move(); 
+    }
+
+    private void Move()
+    {
+        //var is a generic variable which changes its type to according to value
+        //deltaX is the difference the Player Ship moves in the x-axis.
+         var deltaX= Input.GetAxis("Horizontal");
+        //newXpostion = current position was in x = the difference moved in x-axis. 
+         var newXPos = transform.position.x +deltaX;
+        //move the player ship on the x-axis only. (newXPos) 
+        transform.position = new Vector2(newXPos,transform.position.y);
+
         
     }
+
 }
