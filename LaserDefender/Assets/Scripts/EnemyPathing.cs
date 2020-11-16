558998c7130b2876ae6ce0a5cf68592c47eef65f
Daylen2003 <73096438+Daylen2003@users.noreply.github.com>
2020-11-16 09:49:57 +0100

    Setting Axes of ship.
    We have set the lasers to stop at a collision and set the waypoints so that the enemy goes from waypoint.

---
+++ LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour
+{
+    //Transform because our list is made from x and y-axis. 
+    [SerializeField] List<Transform> waypointsList;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Set the position of the enemy ship to the 1st waypoint
+        transform.position = waypointsList[0].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ LaserDefender/Assets/Scripts/Shredder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shredder : MonoBehaviour
+    // Other object is a variable name and the gameObject is the laser.
+    // The variable is created in the void. 
+{
+    private void OnCollisionEnter2D(Collision2D otherObject)
+    {
+        print(otherObject.gameObject.name);
+    }
+
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        Destroy(otherObject.gameObject); 
+    }
+}
