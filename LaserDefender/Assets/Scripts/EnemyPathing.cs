223e863d27c5859b2621bcc9b567d2eb29a19ca2
Daylen2003 <73096438+Daylen2003@users.noreply.github.com>
2020-11-18 16:21:01 +0100

    Set up of waypoints
    waypoints, making the player move and destroyed and we have set up the wav files.

--- LaserDefender/Assets/Scripts/EnemyPathing.cs
+++ LaserDefender/Assets/Scripts/EnemyPathing.cs
@@ -4,18 +4,51 @@ using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour
 {
+    // A list of type transform as waypoints are at postions in x and y. 
     //Transform because our list is made from x and y-axis. 
     [SerializeField] List<Transform> waypointsList;
+    [SerializeField] float enemeyMoveSpeed = 2f;
+    // shows the next waypoint
+    int waypointIndex = 0;
     // Start is called before the first frame update
     void Start()
     {
-        // Set the position of the enemy ship to the 1st waypoint
-        transform.position = waypointsList[0].transform.position;
+        // Set the starting position of the enemy ship to the 1st waypoint
+        transform.position = waypointsList[waypointIndex].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        EnemyMove();
+    }
+
+    private void EnemyMove()
+    {
+        // 0 , 1 , 2     <       3
+        if(waypointIndex < waypointsList.Count)
+        {
+            // Set the target position to the next waypoint position.
+            //  Tragetposition where we want to go.
+            var targetPosition = waypointsList[waypointIndex].transform.position;
+            
+            targetPosition.z = 0f;
+            //enemyMovement per frame 
+            var enemnyMovement = enemeyMoveSpeed * Time.deltaTime;
+
+            //move from current position, to target position, at enemy movement speed
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, enemnyMovement);
+
+            // check if we reached the new target position
+            if(transform.position == targetPosition)
+            {
+                waypointIndex++;
+            }
+        }
+        //if enemy reached last waypoint
+        else
+        {
+            Destroy(gameObject); 
+        }
     }
 }
---
+++ LaserDefender/Assets/Scripts/WavConvig.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(menuName = "Enemy wave config")]
+public class WavConvig : ScriptableObject
+{
+    //the enemy sprite
+    [SerializeField] GameObject enemyPrefeb;
+
+    //the path to follow
+    [SerializeField] GameObject pathPrefeb;
+
+    //time between enemy spawn
+    [SerializeField] float timeBetweenSpawns = 0.5f;
+
+    //random time difference between spawns
+    [SerializeField] float spawnRandomfactor = 0.3f;
+
+    //number of enemies in wav
+    [SerializeField] int numberOfEnemies = 5;
+
+    //enemy movement speed
+    [SerializeField] float EnemyMovement = 2f;
+
+}
