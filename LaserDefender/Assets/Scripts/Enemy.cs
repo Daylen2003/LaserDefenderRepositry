7c4eba7015df040c6f74c25812593a637a1ef944
Daylen2003 <73096438+Daylen2003@users.noreply.github.com>
2020-12-02 16:30:21 +0100

    Shooting lasers and dealing damage
    Shooting lasers to both enemy and player and also dealing damage.

---
+++ LaserDefender/Assets/Scripts/Enemy.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+
+    [SerializeField] float health = 100;
+
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+
+    [SerializeField] GameObject enemyLaserPrefab;
+    [SerializeField] float enemeyLaserSpeed = 5f; 
+
+    // otherObject is a variable name, reduce enemy health whenever enemy collides with a gameObject that have a damage dealer component. 
+    private void OnTriggerEnter2D(Collider2D otherObject)
+    {
+        // access damage dealer from other object that hit the enemy, and reduce health accordingly.
+        DamageDealer dmg = otherObject.gameObject.GetComponent<DamageDealer>();
+        ProcessHit(dmg);
+    }
+
+    private void ProcessHit(DamageDealer dmg) 
+    {
+        health -= dmg.GetDamage();
+
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get a random number
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CountDownAndShoot();
+    }
+
+    private void CountDownAndShoot()
+    {
+        // reduce time every frame
+        shotCounter -= Time.deltaTime;
+
+        if(shotCounter <= 0f)
+        {
+            //enemy shoots
+            EnemyFire();
+            // reset shot counter timer
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+    private void EnemyFire()
+    {
+        GameObject laser = Instantiate(enemyLaserPrefab, transform.position, Quaternion.identity) as GameObject;
+        // Give the laser a velocity in the Y-axis.
+        laser.GetComponent<Rigidbody2D>().velocity = new Vector2(0,-enemeyLaserSpeed);
+    }
+}
