3f95ef9f8261d0827144a07f3d9a199247a83d6a
Daylen2003 <73096438+Daylen2003@users.noreply.github.com>
2021-01-18 09:08:16 +0100

    Laser defender done.
    The game is done.

--- LaserDefender/Assets/Scripts/HealthDisplay.cs
+++ LaserDefender/Assets/Scripts/HealthDisplay.cs
@@ -7,16 +7,21 @@ public class HealthDisplay : MonoBehaviour
 {    
     Text healthText;
     Player player;
+    Slider healthSlider;
     // Start is called before the first frame update
     void Start()
     {
        healthText = GetComponent<Text>();
         player = FindObjectOfType<Player>();
+
+        healthSlider = FindObjectOfType<Slider>();
+        healthSlider.maxValue = player.GetHealth();
     }
 
     // Update is called once per frame
     void Update()
     {
         healthText.text = player.GetHealth().ToString();
+        healthSlider.value = player.GetHealth(); 
     }
 }
